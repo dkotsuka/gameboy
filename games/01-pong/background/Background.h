ee10b4e6323773f61c1329e5e0f5ea32c2f17fed
dkotsuka <dkotsuka@gmail.com>
2024-06-16 20:21:22 -0300

    game(01-pong): add collision function

--- games/01-pong/background/Background.h
+++ games/01-pong/background/Background.h
@@ -5,10 +5,9 @@
 
 void render_background(void);
 
-#ifndef CHECK_WALL_COLLISION
-#define CHECK_WALL_COLLISION
 void check_wall_collision(Object *object, uint8_t future_pos_x, uint8_t future_pos_y, BYTE change_direction);
-#endif // CHECK_WALL_COLLISION
+BYTE check_collision_between(Object *object1, Object *object2);
+BYTE calculate_collision(Area2D area1, Area2D area2);
 
 void change_object_dir_x(Object *object, BYTE change_dir);
 void change_object_dir_y(Object *object, BYTE change_dir);
